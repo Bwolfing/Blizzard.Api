4293d532d01220dab3fdfb6f0c945722447b2ddb
Bwolfing <wolfington98@gmail.com>
2017-05-02 07:33:25 -0700

    Added WoWCommunity methods

--- Blizzard.Api.Clients/IWowCommunity.cs
+++ Blizzard.Api.Clients/IWowCommunity.cs
@@ -1,6 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Blizzard.Api.Data.WoW;
+
 namespace Blizzard.Api.Clients
 {
     public interface IWowCommunity
     {
+        Task<Achievement> GetAchievementAsync(int id);
+        Task<Character> GetCharacterProfileAsync(string realm, string characterName, params string[] fields);
+        Task<Guild> GetGuildProfileAsync(string realm, string guildName, params string[] fields);
+        Task<Item> GetItemAsync(int id);
+        Task<List<RaceDataResource>> GetCharacterRacesAsync();
+        Task<List<ItemClass>> GetItemClassesAsync();
+        Task<List<RealmStatus>> GetRealmStatusesAsync(params string[] realms);
     }
 }
---
+++ Blizzard.Api.Data/WoW/RaceDataResource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blizzard.Api.Data.WoW.Enums;
+using Newtonsoft.Json;
+
+namespace Blizzard.Api.Data.WoW
+{
+    public class RaceDataResource
+    {
+        public int Id { get; set; }
+
+        public int Mask { get; set; }
+
+        [JsonProperty("id")]
+        public Race Race { get; set; }
+
+        [JsonProperty("side")]
+        public Faction Faction { get; set; }
+    }
+}
